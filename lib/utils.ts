2c093bec6f82d3e7dbfdf691811e7bd38c9ffad1
gaurav <gaurav@tokenmetrics.com>
2024-07-05 21:54:01 +0530

    changed config

--- lib/utils.ts
+++ lib/utils.ts
@@ -201,7 +201,7 @@ export const authFormSchema = (type: string) => z.object({
   lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
   city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
-  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(20),
   postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
   dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
